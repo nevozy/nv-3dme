345f956b0ec62621ae9f012be4d5da0f34001f84
nevozy <nevo020806@gmail.com>
2020-09-25 14:30:31 +0300

    Update js.js

--- html/js.js
+++ html/js.js
@@ -1,12 +1,6 @@
 
 addEventListener("message", function(event) {
-    if (event.data.data) {
-        if (event.data.data == "") {
-            $("#3dme").fadeOut(500);
-        } else {
-            $("#3dme").fadeIn(500);
-        }
-
+    if (event.data.data || event.data.data == "") {
         $("#3dme").html(event.data.data)
     }
 });
